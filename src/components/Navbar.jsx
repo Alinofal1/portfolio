1f5eb9416727002773921fe949a5d1f706d34e50
Alinofal1 <alinofal405@gmail.com>
2026-04-11 15:30:44 +0300

    added new projects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
-import { Link } from 'react-router-dom';
-import logo from '../assets/aliLogo.png'; 
-import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import logo from "../assets/aliLogo.png";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img src={logo} className='mx-2 w-32' alt="logo" />
+        <img src={logo} className="mx-2 w-32" alt="logo" />
       </div>
-      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <Link to={'https://www.linkedin.com/in/ali-nofal-ba4583327/'} target='_blank'>
-        <FaLinkedin className='hover:text-purple-800 cursor-pointer duration-300'/>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <Link to={"https://www.linkedin.com/in/ali-noffal/"} target="_blank">
+          <FaLinkedin className="hover:text-purple-800 cursor-pointer duration-300" />
         </Link>
-        <Link to={'https://github.com/Alinofal1'} target='_blank'>
-        <FaGithub className='hover:text-purple-800 cursor-pointer duration-300' />
+        <Link to={"https://github.com/Alinofal1"} target="_blank">
+          <FaGithub className="hover:text-purple-800 cursor-pointer duration-300" />
         </Link>
       </div>
     </nav>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,17 +1,18 @@
 import project1 from "../assets/projects/netflix.png";
-import project2 from "../assets/projects/media-plus.png";
-import project3 from "../assets/projects/orient.png";
-import project4 from "../assets/projects/ohchr.png";
-import project5 from "../assets/projects/give-palestine.png";
-import project6 from "../assets/projects/burger-hub.png";
-import project7 from "../assets/projects/user-dashboard.png";
-import project8 from "../assets/projects/streamerzz.png";
-import project9 from "../assets/projects/3d-project.png";
-import project10 from "../assets/projects/cruds.jpg";
-import project11 from "../assets/projects/memory-game.png";
-import project12 from "../assets/projects/blue-print.png";
-import project13 from "../assets/projects/Eshop.png";
-import project14 from "../assets/projects/bootstrap.png";
+import project2 from "../assets/projects/real-estate.png";
+import project3 from "../assets/projects/media-plus.png";
+import project4 from "../assets/projects/orient.png";
+import project5 from "../assets/projects/ohchr.png";
+import project6 from "../assets/projects/give-palestine.png";
+import project7 from "../assets/projects/burger-hub.png";
+import project8 from "../assets/projects/user-dashboard.png";
+import project9 from "../assets/projects/streamerzz.png";
+import project10 from "../assets/projects/3d-project.png";
+import project11 from "../assets/projects/cruds.jpg";
+import project12 from "../assets/projects/memory-game.png";
+import project13 from "../assets/projects/blue-print.png";
+import project14 from "../assets/projects/Eshop.png";
+import project15 from "../assets/projects/bootstrap.png";
 
 export const HERO_CONTENT = `I'm Ali Nofal, a skilled front-end developer specializing in React.js and Next.js. With a passion for creating modern, high-performance web applications, I build user-friendly, responsive, and efficient digital experiences. My portfolio showcases a variety of projects that highlight my expertise in front-end development, and performance optimization. Let's bring your ideas to life with cutting-edge web solutions!`;
 
@@ -51,9 +52,18 @@ export const PROJECTS = [
     technologies: ["React", "Tailwind", "FireBase"],
   },
   {
+    title: "Real Estate",
+    link: "https://real-estate-v01.netlify.app/",
+    image: project2,
+    description: `Developed a modern and fully responsive real estate web application from scratch using React.js and Tailwind CSS, focusing on delivering a clean, user-friendly, and visually engaging experience.
+
+The platform allows users to explore properties, browse listings, and interact with dynamic UI components such as search filters, categories, and featured listings, creating a smooth and intuitive user journey.`,
+    technologies: ["React", "Tailwind", "Framer Motion", "Swiper Js"],
+  },
+  {
     title: "Media Plus",
     link: "https://media-plus-v1.netlify.app/",
-    image: project2,
+    image: project3,
     description:
       "Built Media Plus, a Ramallah-based multimedia agency website, from scratch using HTML, CSS, and JavaScript. Integrated LightGallery for interactive media galleries and Swiper.js for responsive sliders. Focused on storytelling and branding, creating a visually engaging and fully responsive experience for NGO and private clients.",
     technologies: ["HTML", "CSS", "JavaScript", "LightGallery", "Swiper Js"],
@@ -61,7 +71,7 @@ export const PROJECTS = [
   {
     title: "Orient",
     link: "https://orient-v1.netlify.app/",
-    image: project3,
+    image: project4,
     description:
       "Developed Orient, a real desktop-only website for a Palestinian company from scratch using HTML, CSS, and JavaScript. Integrated LightGallery for interactive media displays and Swiper.js for seamless sliders. Optimized layouts and animations to deliver a smooth, professional user experience that enhances branding and engagement",
     technologies: ["HTML", "CSS", "JavaScript", "LightGallery", "Swiper Js"],
@@ -69,7 +79,7 @@ export const PROJECTS = [
   {
     title: "OHCHR Qatar",
     link: "https://ohchr.netlify.app/",
-    image: project4,
+    image: project5,
     description:
       "Developed OHCHR Qatar, a real desktop-only website from scratch using HTML, CSS, and JavaScript, for a human rights organization in Qatar. Implemented clear navigation, structured content, interactive media galleries with LightGallery, and dynamic sliders using Swiper.js. Delivered a polished and professional user experience that enhances accessibility and engagement with the organization’s resources.",
     technologies: ["HTML", "CSS", "JavaScript", "LightGallery", "Swiper Js"],
@@ -77,7 +87,7 @@ export const PROJECTS = [
   {
     title: "Give Palestine",
     link: "https://give-palestine.netlify.app/",
-    image: project5,
+    image: project6,
     description:
       "Developed Give Palestine, a real desktop-only website from scratch using HTML, CSS, and JavaScript, supporting Palestinian communities through educational, cultural, and humanitarian initiatives. Implemented clear navigation, structured and informative content, interactive media galleries with LightGallery, and dynamic sliders using Swiper.js. Delivered a polished and engaging user experience that enhances accessibility and user interaction with the initiatives.",
     technologies: ["HTML", "CSS", "JavaScript", "LightGallery", "Swiper Js"],
@@ -85,7 +95,7 @@ export const PROJECTS = [
   {
     title: "food app",
     link: "https://burger-hubb.netlify.app/",
-    image: project6,
+    image: project7,
     description:
       "Built Burger Hubb, a responsive web application from scratch using React and Tailwind CSS. Designed a clean and interactive interface showcasing a diverse menu of burgers, sides, and beverages. Implemented smooth navigation, reusable components, and a scalable front-end architecture to deliver an engaging and polished user experience.",
     technologies: ["React", "Tailwind Css"],
@@ -93,7 +103,7 @@ export const PROJECTS = [
   {
     title: "User Dashboard",
     link: "https://user-dashboard2.netlify.app/",
-    image: project7,
+    image: project8,
     description:
       "Built a modern and responsive User Dashboard from scratch using Next.js, Tailwind CSS, and Chart.js. Designed a clean and interactive interface with dynamic charts and organized layouts, enabling efficient visualization and management of data. Focused on reusable components and scalable architecture to deliver a polished and professional user experience.",
     technologies: ["Next Js", "Tailwind Css", "Chart Js"],
@@ -101,7 +111,7 @@ export const PROJECTS = [
   {
     title: "Streamerzz web app",
     link: "https://streamerzz-an.netlify.app/",
-    image: project8,
+    image: project9,
     description:
       "Built Streamerzz, a modern streaming platform UI from scratch using React, Tailwind CSS, Framer Motion, and React-Scroll. Designed a clean, responsive, and interactive interface showcasing dynamic movie listings and smooth user interactions. Focused on reusable components and scalable front-end architecture to deliver a polished and engaging user experience.",
     technologies: ["React", "Tailwind Css", "Framer Motion", "React-scroll"],
@@ -109,7 +119,7 @@ export const PROJECTS = [
   {
     title: "3D Project",
     link: "https://3d-projectt.netlify.app/",
-    image: project9,
+    image: project10,
     description:
       "Built a creative 3D web experience from scratch using HTML, CSS, and Spline. Developed an interactive robot that smoothly follows the user's cursor, creating an engaging and immersive experience. Focused on responsive design, smooth animations, and scalable front-end structure for a polished and professional interface.",
     technologies: ["Html", "Css", "Spline"],
@@ -117,7 +127,7 @@ export const PROJECTS = [
   {
     title: "CRUD PRODUCT MANAGEMENT SYSTEM",
     link: "https://prodcts-management-system.netlify.app/",
-    image: project10,
+    image: project11,
     description:
       "Built a Product Management System from scratch using HTML, CSS, Bootstrap, and JavaScript. Implemented CRUD operations to manage product records with a responsive and interactive interface. Focused on clean UI, intuitive navigation, and scalable front-end architecture for a polished and professional user experience.",
     technologies: ["HTML", "CSS", "Bootstrap", "JavaScript"],
@@ -125,7 +135,7 @@ export const PROJECTS = [
   {
     title: "Memory Game",
     link: "https://memory-gamme.netlify.app/",
-    image: project11,
+    image: project12,
     description:
       "Built a Memory Game from scratch using HTML, CSS, and React.js. Developed an interactive and visually engaging interface where players match pairs of cards through a series of flips. Focused on responsive design, smooth animations, and reusable components to deliver a polished and enjoyable user experience.",
     technologies: ["HTML", "CSS", "React"],
@@ -133,7 +143,7 @@ export const PROJECTS = [
   {
     title: "Blue-Print360",
     link: "https://blueprint-360.com/",
-    image: project12,
+    image: project13,
     description:
       "Developed Blueprint 360, a real responsive platform for Blueprint Company from scratch using React.js and Tailwind CSS. Designed a modern and visually engaging interface showcasing the company’s tech consulting services, optimized for all devices. Focused on reusable components and scalable architecture to deliver a polished and professional user experience.",
     technologies: ["HTML", "Tailwind", "ReactJs"],
@@ -141,7 +151,7 @@ export const PROJECTS = [
   {
     title: "Eshop",
     link: "https://alinofal-eshop.netlify.app/",
-    image: project13,
+    image: project14,
     description:
       "Built a modern and fully responsive e-commerce web application from scratch using React and Tailwind CSS. Developed dynamic product listings and an intuitive interface with smooth user interactions. Focused on reusable components, scalable architecture, and polished design to deliver a seamless and engaging browsing experience.",
     technologies: ["HTML", "Tailwind", "ReactJs"],
@@ -149,7 +159,7 @@ export const PROJECTS = [
   {
     title: "Bootstrap project",
     link: "https://bootstrab-app.netlify.app/",
-    image: project14,
+    image: project15,
     description:
       "Built a responsive web application from scratch using HTML and Bootstrap. Designed a sleek and modern interface showcasing effective use of Bootstrap components and layout systems. Focused on polished visuals, smooth interactions, and responsive design to deliver a professional and engaging user experience.",
     technologies: ["HTML", "Bootstrab"],
